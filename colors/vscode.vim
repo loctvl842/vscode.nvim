b0dc070c07b06a2b457753a12cedac2dd4213267
locletvl842002 <loc.letvl@hcmut.edu.vn>
2022-06-07 18:55:26 +0700

    vscode dark theme for neovim

---
+++ colors/vscode.vim
@@ -0,0 +1,25 @@
+lua << EOF
+package.loaded['vscode'] = nil
+package.loaded['vscode.highlights'] = nil
+package.loaded['vscode.Treesitter'] = nil
+package.loaded['vscode.markdown'] = nil
+package.loaded['vscode.Whichkey'] = nil
+package.loaded['vscode.Git'] = nil
+package.loaded['vscode.LSP'] = nil
+package.loaded['vscode.Quickscope'] = nil
+package.loaded['vscode.Telescope'] = nil
+package.loaded['vscode.NvimTree'] = nil
+package.loaded['vscode.Lir'] = nil
+package.loaded['vscode.Buffer'] = nil
+package.loaded['vscode.StatusLine'] = nil
+package.loaded['vscode.IndentBlankline'] = nil
+package.loaded['vscode.Dashboard'] = nil
+package.loaded['vscode.DiffView'] = nil
+package.loaded['vscode.Bookmarks'] = nil
+package.loaded['vscode.Bqf'] = nil
+package.loaded['vscode.Cmp'] = nil
+package.loaded['vscode.SymbolOutline'] = nil
+package.loaded['vscode.Misc'] = nil
+
+require("vscode")
+EOF
---
+++ lua/vscode/NeoTree.lua
@@ -0,0 +1,21 @@
+local NeoTree = {
+	NeoTreeNormal = { fg = C.light_gray, bg = C.tree_gray },
+	NeoTreeNormalNC = { fg = C.light_gray, bg = C.tree_gray },
+	-- NeoTreeNormalNC = { fg = C.light_gray, bg = C.bg },
+	NeoTreeSignColumn = { fg = C.light_gray, bg = C.tree_gray },
+	NeoTreeEndOfBuffer = { fg = C.tree_gray, bg = C.tree_gray },
+	NeoTreeStatusLine = { fg = C.tree_gray, bg = "#007acc" },
+	NeoTreeCursorLine = { bg = C.cursor_line },
+	-- NeoTreeWinSeparator = { fg = C.bg, bg = C.bg },
+	NeoTreeWinSeparator = { fg = C.tree_gray, bg = C.tree_gray },
+	NeoTreeRootName = { style = "bold" },
+	NeoTreeDirectoryIcon = { fg = C.dir_icon },
+	NeoTreeDirectoryName = { fg = C.dir_name },
+	NeoTreeGitModified = { fg = C.git_modified },
+	-- NeoTreeGitModified = { fg = "#ff0000" },
+	NeoTreeGitUntracked = { fg = C.git_untracked },
+	NeoTreeIndentMarker = { fg = C.indent_marker },
+	NeoTreeExpander = { fg = C.expander },
+}
+
+return NeoTree
